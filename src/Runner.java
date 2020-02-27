d35cb0183d821be03719ec2635467c458ae5001f
tonymontana <Luckytoha333@gmail.com>
2020-02-27 20:50:32 +0300

    add PurchaseTask

--- src/Runner.java
+++ src/Runner.java
@@ -1,63 +1,74 @@
-import by.gsu.pms.Employee;
+import by.gsu.pms.SortedByCost;
+import by.gsu.pms.SortedByName;
+import by.gsu.pms.WeekDay;
+import by.gsu.pms.Purchase;
+
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
 
 public class Runner {
 
-    private static int initialTrips(Employee[] employee) {
-        int duration = 0;
-        for (int i = 0; i < employee.length; i++) {
-            if (employee[i] != null) {
-                for (int j = i + 1; j < employee.length; j++) {
-                    if (employee[j] != null) {
-                        if (employee[i].toString().equals(employee[j].toString())) duration = employee[i].getDays();
-                    }
-                }
-            }
-        }
-        return duration;
+    private static WeekDay maxCostDay(Purchase[] purchase) {
+        Purchase maxCost = purchase[0];
+        for (Purchase item : purchase)
+            if (item != null)
+                if (item.getTotal().equals(item.getTotal().max(maxCost.getTotal()))) maxCost = item;
+        return maxCost.getDay();
     }
 
-    private static BigDecimal totalExpenses(Employee[] employee) {
-        BigDecimal total = new BigDecimal("0");
-        for (Employee item : employee) {
+    private static BigDecimal mondayCost(Purchase[] purchase) {
+        BigDecimal cost = new BigDecimal(0);
+        for (Purchase item : purchase)
             if (item != null)
-                total = total.add(item.getTotal());
-        }
-        return total;
+                if (item.getDay() == WeekDay.MONDAY)
+                    cost = cost.add(item.getTotal());
+        return cost;
     }
 
-    private static String maximumTotal(Employee[] employee) {
-        Employee maximum = employee[0];
-        for (Employee item : employee) {
+    private static BigDecimal averageCost(Purchase[] purchase) {
+        BigDecimal cost = new BigDecimal(0);
+        for (Purchase item : purchase)
             if (item != null)
-                if (item.getTotal().equals(item.getTotal().max(maximum.getTotal()))) maximum = item;
-        }
-        return maximum.getAccount();
+                cost = cost.add(item.getTotal());
+        return cost.divide(BigDecimal.valueOf(purchase.length), RoundingMode.HALF_UP);
     }
 
     public static void main(String[] args) {
-        Employee[] employee = new Employee[7];
-        employee[0] = new Employee("Siroga Krasiviy", 800, 2.21, 2, 1);
-        employee[1] = new Employee( "Yarik Starik", 600, 2.21, 2, 7);
-        employee[3] = new Employee( "Romka Kovbasa", 300, 2.21, 2, 21);
-        employee[4] = new Employee( "Drozdik Pashkevich", 750, 2.21, 2, 3);
-        employee[5] = new Employee( "Yarik Starik", 600, 2.21, 2, 7);
-        employee[6] = new Employee();
+        Purchase[] purchase = new Purchase[7];
+        purchase[0] = new Purchase(2.52, "Banana", WeekDay.MONDAY, 10);
+        purchase[1] = new Purchase(1.566, "Apple", WeekDay.FRIDAY, 20);
+        purchase[2] = new Purchase(1.23, "Lemon", WeekDay.THURSDAY, 3);
+        purchase[3] = new Purchase(4.23, "Pineapple", WeekDay.TUESDAY, 5);
+        purchase[4] = new Purchase(2.366, "Orange", WeekDay.WEDNESDAY, 15);
+        purchase[5] = new Purchase(5.28, "Mango", WeekDay.SATURDAY, 8);
+        purchase[6] = new Purchase(1.899, "Kiwi", WeekDay.MONDAY, 7);
 
-        for (Employee item : employee) {
+        for (Purchase item : purchase)
             if (item != null) item.show();
-        }
 
-        employee[6].setTransport(300, 2.21, 2);
+        purchase[2].setCost(3.25);
 
-        System.out.println("Duration = " + initialTrips(employee));
-
-        for (Employee item : employee) {
+        for (Purchase item : purchase)
             System.out.println(item);
-        }
 
-        System.out.println(totalExpenses(employee));
-        System.out.println(maximumTotal(employee));
+        System.out.println(maxCostDay(purchase));
+        System.out.println(mondayCost(purchase));
+        System.out.println(averageCost(purchase));
 
+        Arrays.sort(purchase);
+        System.out.println("Sorted by count:");
+        for (Purchase item : purchase)
+            System.out.println(item);
+
+        Arrays.sort(purchase, new  SortedByCost());
+        System.out.println("Sorted by cost:");
+        for (Purchase item : purchase)
+            System.out.println(item);
+
+        Arrays.sort(purchase, new SortedByName());
+        System.out.println("Sorted by name:");
+        for (Purchase item : purchase)
+            System.out.println(item);
     }
 }
